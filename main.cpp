7bf7c3d3e2924ee702b337f06d6eb5e8dbfd68e0
Safaalofi <safaalofi9@gmail.com>
2026-08-24 19:16:06 +0400

    parsed the location data

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
-#include<iostream>
-#include <"C:\Users\safaa\Desktop\httplib.h">
-#include<"C:\Users\safaa\Desktop\json.hpp">
+#include<iostream> 
+#define CPPHTTPLIB_OPENSSL_SUPPORT // enables HTTPS in httplib
+// define comes before include httplib
+#include "C:\Users\safaa\Desktop\httplib.h" // allows the c++ program to communicate with web servers
+#include "C:\Users\safaa\Desktop\json.hpp"
+
+int main(){
+    // create an http client called client
+    httplib:: Client client ("https://api.wheretheiss.at");
+    // send a GET request 
+    if (auto res = client.Get("/v1/satellites/25544")){
+        std::cout<< res->status <<std::endl;
+        std::cout<<res->body<<std::endl;
+    }
+
+}
